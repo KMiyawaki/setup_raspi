e9bd5e584a7cc4e5fd67d8b4e23ae747adfdf2e9
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2025-03-03 16:16:39 +0900

    fix: x11vnc installer

--- install_x11vnc.sh
+++ install_x11vnc.sh
@@ -2,23 +2,7 @@
 
 sudo apt-get install -y x11vnc
 sudo x11vnc -storepasswd /etc/.vncpasswd
-sudo touch /etc/systemd/system/x11vnc.service
-sudo cat > /etc/systemd/system/x11vnc.service << EOF
-[Unit]
-Description=x11vnc (Remote access)
-After=network-online.target
-
-[Service]
-Type=simple
-ExecStart=/usr/bin/x11vnc -auth guess -display :0 -rfbauth /etc/.vncpasswd -rfbport 5900 -forever -loop -noxdamage -repeat -shared
-ExecStop=/bin/kill -TERM $MAINPID
-ExecReload=/bin/kill -HUP $MAINPID
-KillMode=control-group
-Restart=on-failure
-
-[Install]
-WantedBy=graphical.target
-EOF
+sudo cp ./x11vnc.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable x11vnc
 sudo systemctl start x11vnc
